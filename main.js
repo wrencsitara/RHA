7f6ea58c61b6a6bf6f5548268bfe1e7a92192383
wrencsitara <154820184+wrencsitara@users.noreply.github.com>
2026-03-02 03:25:30 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,4 @@
-onload = () =>{
-        document.body.classList.remove("container");
-};
 function showLetter() {
-  const letter = document.getElementById("letterBox");
-  letter.style.display = "block";
-
-  const music = document.getElementById("bgMusic");
-  if (music.paused) {
-    music.play();
-  }
+  document.getElementById("letterBox").style.display = "block";
+  document.getElementById("flowerBtn").style.display = "block";
 }
-
-window.addEventListener("click", () => {
-  const music = document.getElementById("bgMusic");
-  if (music.paused) {
-    music.play();
-  }
-});
